048e3abdd6ad1f0457a6eeecefe2130f7eeb6716
Steve Foster <sfoster2008@cox.net>
2018-02-05 20:47:45 -0800

    remove files

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,6 @@
+
 //Create an array of Words
-var element = ["hydrogen", "helium", "nitrogen", "carbon", "oxygen", "neon", "xenon", "radon", "iron", "tin", "flourine", "sodium"];
+var element = ["hydrogen", "helium", "nitrogen", "carbon", "oxygen", "argon", "xenon", "radon", "iron", "tin", "flourine", "sodium"];
 
 //Choose word randomly
 var randNum = Math.floor(Math.random()*element.length);
@@ -26,24 +27,24 @@ let generateunderScore = () => {
 console.log(generateunderScore());
 
 //Get users guess
-document.addEventListener('keypress', (event) => {
-    let keyword = String.fromCharCode(event.keyCode);
-    console.log(event);
-// if users guess is right
-    if(chosenWord.indexOf(keyword) > -1) {
-    //add to right word array
-        rightWord.push(keyword);
-    //replace underScore with right letter    
-        underScore[chosenWord.indexOf(keyword)] = keyword;
-        docUnderScore[0].innerHTML = underScore.join(' ');
-        docRightGuess[0].innerHTML = rightWord;            
-    //check to see if user work matches guesses
-        if(underScore.join('') == chosenWord) {
-            alert('You Win!');
+    document.addEventListener('keypress', (event) => {
+        let keyword = String.fromCharCode(event.keyCode);
+        console.log(event);
+    // if users guess is right
+        if(chosenWord.indexOf(keyword) > -1) {
+        //add to right word array
+            rightWord.push(keyword);
+        //replace underScore with right letter    
+            underScore[chosenWord.indexOf(keyword)] = keyword;
+            docUnderScore[0].innerHTML = underScore.join(' ');
+            docRightGuess[0].innerHTML = rightWord;            
+        //check to see if user work matches guesses
+            if(underScore.join('') == chosenWord) {
+                alert('You Win!');
+            }
         }
-    }
-    else {
-        wrongWord.push(keyword);
-        docWrongGuess[0].innerHTML = wrongWord;
-    }
-});
+        else {
+            wrongWord.push(keyword);
+            docWrongGuess[0].innerHTML = wrongWord;
+        }
+    });
